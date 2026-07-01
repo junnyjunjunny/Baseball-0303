9d33a9a0e5f68ae366f709ab3748affa276198c7
hyunjun <chlguswns96@gmail.com>
2026-07-01 15:07:33 +0900

    [feature] 52

--- test_game.py
+++ test_game.py
@@ -1,5 +1,6 @@
 import pytest
 from game import Game
+from game_result import GameResult
 
 
 @pytest.fixture
@@ -14,3 +15,12 @@ def assert_illegal_argument(game: Game, guessNumber: str):
 @pytest.mark.parametrize("invalid_input", [None, "12", "1234", "abc", "1b3", "121"])
 def test_exception_when_invalid_input(game, invalid_input : str):
     assert_illegal_argument(game, invalid_input)
+
+def test_return_solved_result_if_matched_number(game):
+    game.question = "123"
+    result: GameResult = game.guess("123")
+
+    assert result is not None
+    assert result.solved == True
+    assert result.strikes == 3
+    assert result.balls == 0
